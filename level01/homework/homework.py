ab214f3fd3156807433433bc35a813442a6b7cbf
vazhamgeladze <vazhamgeladze61@gmail.com>
2024-10-04 17:02:17 +0400

    first commit

---
+++ level01/homework/homework.py
@@ -0,0 +1,18 @@
+name = "vazha"
+surname = "mgeladze"
+print(name)
+age = 11
+heigth = 130.6
+knows_programing = True
+is_ugly = False
+print(name +" " + surname)
+name(name + age )
+# print(type(age))
+# print(type(age))
+# print (type(name))
+# print(type(surname))
+#print(type(heigth))
+# rint(type(knows_programing))
+print(name + str (age))
+
+witelquda = 1
