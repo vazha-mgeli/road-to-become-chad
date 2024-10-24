3a0d6110936d9f3b22d1feea6b4ce64b2ae92767
vazhamgeladze <vazhamgeladze61@gmail.com>
2024-10-24 18:26:44 +0400

    commit

---
+++ level09/homework/09 9.py
@@ -0,0 +1,50 @@
+if 5 > 3: # False
+    print("vaja")
+    print("demetre")
+elif 3 < 2:
+    print("es tsinadadeba aris martali")
+else: # false
+    print("3 naklebi ar aris 2 ze")
+
+if 5 < 2: # False
+    print("false")
+elif 8 < 5 :
+    print("me ra vici")
+
+if 5 < 7 :
+    print("ეს წინადადება არასწორია")
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<center>
+    <br><br><br>
+    <form>
+        <input type="text" placeholder="სახელი" name="saxeli">
+        <br><br>
+        <input type="text" placeholder="გვარი" name="gvari">
+        <br><br>
+        <input type="email" placeholder="email" name="email">
+        <br><br>
+        <input type="password" placeholder="password" name="password">
+        <br><br>
+
+        <select name="text">
+            <option value="sandro">sandro</option>
+            <option value="sandro">vaza</option>
+            <option value="sandro">dato</option>
+        </select>
+
+    </form>
+
+
+</center>
+
+
+
+</body>
+</html>
